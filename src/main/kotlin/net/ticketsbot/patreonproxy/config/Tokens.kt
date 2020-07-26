761c056a9e6f8c3bc13a59928ad2118df99fe7c7
rxdn <dotrar@protonmail.com>
2020-07-26 14:49:24 +0100

    fix npe

--- src/main/kotlin/net/ticketsbot/patreonproxy/config/Tokens.kt
+++ src/main/kotlin/net/ticketsbot/patreonproxy/config/Tokens.kt
@@ -1,8 +1,14 @@
 package net.ticketsbot.patreonproxy.config
 
+// can't get dynamically from an envvar, sorry
 @FileName("/data/tokens.json")
 class Tokens : JSON() {
-    val accessToken = this.config.getGenericOrNull<String>("access_token")
-    val refreshToken = this.config.getGenericOrNull<String>("refresh_token")
-    val expires = this.config.getGenericOrNull<Long>("access_token")
+    val accessToken: String?
+
+        get() = this.config.getGenericOrNull<String>("access_token")
+    val refreshToken: String?
+        get() =  this.config.getGenericOrNull<String>("refresh_token")
+
+    val expires: Long?
+        get() =  this.config.getGenericOrNull<Long>("access_token")
 }
